2959ee7327742cb3ac84b2233de89bf321f30929
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2025-02-24 13:13:38 +0900

    ポップアップ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,9 +2,14 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([35.675888, 139.744858], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([35.675888, 139.744858]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
